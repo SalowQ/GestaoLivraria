67f6812568402eb5a66f526ed6595e30d7e89d8f
Igor Sousa <arqurb.igor@gmail.com>
2025-05-21 13:15:35 -0300

    feat: criar classe livro

---
+++ Book.cs
@@ -0,0 +1,25 @@
+﻿namespace GestaoLivraria
+{
+    public enum BookGenre
+    {
+        Fiction,
+        NonFiction,
+        Science,
+        History,
+        Fantasy,
+        Mystery,
+        Romance,
+        Thriller,
+        Horror,
+        Biography
+    }
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public BookGenre Genre { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
